30d9f5a5e3b945c46f78ed2dfed881d669bdb572
Purvesh Makode <makodepurvesh@gmail.com>
2020-06-02 23:23:34 +0530

    Initial commit/Add React componets-1/components2/reactstrap support

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
+
+import {
+  Jumbotron,
+  Button,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  Collapse,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap';
+
+import MenuComponent from './components/MenuComponent';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { DISHES } from './shared/dishes';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this)
+
+    this.state = {
+      dishes: DISHES,
+      isOpen: false
+    };
+
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+
+
+  render() {
+   
+      // const [isOpen, setIsOpen] = useState(false);
+      // const toggle = () => setIsOpen(!isOpen);
+      
+     
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/purveshmakode24" target="_blank">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Jumbotron className="center-jumbo">
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
+
+        {/* My menu component  */}
+        <MenuComponent dishes = {this.state.dishes}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
