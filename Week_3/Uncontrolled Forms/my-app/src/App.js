dea446a0f11ec56c431c05c0c56f3b0d61f51399
Purvesh Makode <makodepurvesh@gmail.com>
2020-06-22 04:53:56 +0530

    Add validation for controlled Forms in Controlled Forms and Uncontrolled forms setup in another dir

---
+++ Week_3/Uncontrolled Forms/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import MainComponent from './components/MainComponent';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <BrowserRouter>
+        <div>
+          <MainComponent />
+        </div>
+      </BrowserRouter>
+    );
+  }
+
+}
+
+export default App;
