d1cfcce12f38910d65bdbe2c199d2e97d406014c
Purvesh Makode <makodepurvesh@gmail.com>
2020-06-25 12:07:23 +0530

    fixed Uncontrolled forms and Add redux configuration in react-redux and redux forms dir

---
+++ Week_3/react-redux and redux forms/my-app/src/components/MainComponent.js
@@ -0,0 +1,71 @@
+//--> This is a Container Component: Which has local state
+
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import About from './AboutComponent';
+import MenuComponent from './MenuComponent';
+import Contact from './ContactComponent';
+import Dishdetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {   //here state is redux state and all the Redux states are available as props
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+}
+
+
+class MainComponent extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+
+    const HomePage = () => {
+      return (
+        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]} />
+      );
+    }
+
+    const DishWithId = ({ match }) => {
+      return (
+        <Dishdetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <Header />
+
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
+          <Route exact path="/menu" component={() => <MenuComponent dishes={this.props.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+
+        <Footer />
+      </div>
+    );
+  }
+
+}
+
+export default withRouter(connect(mapStateToProps)(MainComponent));
